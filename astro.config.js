638d344dc9845fcdbc64652ee0e380ba72b22e80
Alice Cadillon <acadillon@gmail.com>
2025-02-21 13:07:02 +0100

    modif dans astro.config  // Changez 'server' en 'static'
    '

--- astro.config.js
+++ astro.config.js
@@ -3,7 +3,7 @@ import vercel from '@astrojs/vercel';
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  output: 'server',
+  output: 'static',
   adapter: vercel({
     imageService: true,
     webAnalytics: {
