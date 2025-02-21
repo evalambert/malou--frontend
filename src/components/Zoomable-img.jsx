f8f003d7ff494d70f926a07f17205a9f6e222429
Alice Cadillon <acadillon@gmail.com>
2025-02-21 18:48:46 +0100

    basic zoom in zoom out il working with zoomable img component

--- src/components/Zoomable-img.jsx
+++ src/components/Zoomable-img.jsx
@@ -1,10 +1,59 @@
-import React from "react";
+import { useEffect, useRef } from 'react';
+import OpenSeadragon from 'openseadragon';
 
-export default function ZoomableImg({ name }) {
-  return (
-    <div>
-      <h1>Hello, {name}!</h1>
-      <p>Welcome to our Astro + React site!</p>
-    </div>
-  );
+
+export default function ZoomableImg({ url}) {
+
+    const viewerRef = useRef(null);
+
+    useEffect(() => {
+        if (viewerRef.current) {
+            const viewer = new OpenSeadragon({
+                element: viewerRef.current,
+                tileSources: {
+                    type: 'image',
+                    url: url,
+                    crossOriginPolicy: 'Anonymous',
+                    ajaxWithCredentials: false,
+                    loadTilesWithAjax: true,
+                    ajaxHeaders: {
+                        'Access-Control-Allow-Origin': '*'
+                    },
+                    success: function(event) {
+                        console.log("Image loaded successfully");
+                    },
+                    error: function(event) {
+                        console.error("Error loading image:", event);
+                    },
+                    rendererSettings: {
+                        preserveImageSizeOnResize: true,
+                        willReadFrequently: true
+                    }
+                },
+                debugMode: false,
+                showNavigator: false,
+                showZoomControl: false,
+                showHomeControl: false,
+                showFullPageControl: false,
+                defaultZoomLevel: 1,
+                minZoomLevel: 1,
+                maxZoomLevel: 2,
+                preserveImageSizeOnResize: true,
+                constrainDuringPan: true,
+                visibilityRatio: 1,
+                homeFillsViewer: true,
+            });
+
+            return () => {
+                viewer.destroy();
+            };
+        }
+    }, [url]);
+
+
+    return (
+        <div>
+            <div ref={viewerRef} id="openseadragon-viewer" className='w-[80vw] h-[80vh] border border-amber-600'></div>
+        </div>
+    );
 }
