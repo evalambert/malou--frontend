3ca0fcf89e019a7be2c09c620fc38278baf5dddf
Alice Cadillon <acadillon@gmail.com>
2025-02-21 16:47:08 +0100

    react installation

--- astro.config.js
+++ astro.config.js
@@ -2,15 +2,22 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 import tailwindcss from "@tailwindcss/vite";
 
+import react from '@astrojs/react';
+
 export default defineConfig({
-  output: 'server',  // Changez 'server' en 'static'
+  // Changez 'server' en 'static'
+  output: 'server',
+
   adapter: vercel({
     imageService: true,
     webAnalytics: {
       enabled: true
     }
   }),
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [react()]
 });
