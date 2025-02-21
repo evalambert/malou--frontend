213385df22117e85e833051c0f044079c297971d
Alice Cadillon <acadillon@gmail.com>
2025-02-21 13:11:56 +0100

    retrait url variable dans env et nouvelle variable api_url dans strapi.js

--- astro.config.js
+++ astro.config.js
@@ -3,7 +3,7 @@ import vercel from '@astrojs/vercel';
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  output: 'static',
+  output: 'static',  // Changez 'server' en 'static'
   adapter: vercel({
     imageService: true,
     webAnalytics: {
