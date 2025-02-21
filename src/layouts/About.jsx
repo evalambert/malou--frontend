d24ae5f6083eb05603017bbe94bc578a5a8c0efb
evalambert <145194730+evalambert@users.noreply.github.com>
2025-02-21 18:54:09 +0100

    fetch actu list on about.astro + add Actu.jsx + add logic filter eventDate

---
+++ src/layouts/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="p-4 bg-gray-100 rounded-xl shadow-md">
+      <p>Test</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/layouts/Actu.jsx
@@ -0,0 +1,69 @@
+//Actu.jsx
+
+/* const Actu = ({ actus }) => {
+    return (
+      <div>
+        <button>Actualités :</button>
+        <ul>
+          {actus.map((actu) => (
+            <li key={actu.id}>
+              <a href={`/actu/${actu.slug}/`}>{actu.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+  
+export default Actu;
+  
+
+ */
+
+const Actu = ({ actus }) => {
+  const currentDate = new Date(); // Récupérer la date actuelle
+
+  // Filtrer les actualités passées (date < aujourd'hui)
+  const pastEvents = actus.filter((actu) => {
+    const eventDate = new Date(actu.eventDate); // Utiliser eventDate pour la comparaison
+    return eventDate < currentDate;
+  });
+
+  // Filtrer les actualités en cours (date >= aujourd'hui)
+  const inProgressEvents = actus.filter((actu) => {
+    const eventDate = new Date(actu.eventDate); // Utiliser eventDate pour la comparaison
+    return eventDate >= currentDate;
+  });
+
+  return (
+    <div>
+      <button>actualités :</button>
+      <ul>
+        {inProgressEvents.length > 0 ? (
+          inProgressEvents.map((actu) => (
+            <li key={actu.id}>
+              <a href={`/actu/${actu.slug}/`}>{actu.title}</a>
+            </li>
+          ))
+        ) : (
+          <p>Aucune actualité en cours.</p>
+        )}
+      </ul>
+
+      <h3>passées :</h3>
+      <ul>
+        {pastEvents.length > 0 ? (
+          pastEvents.map((actu) => (
+            <li key={actu.id}>
+              <a href={`/actu/${actu.slug}/`}>{actu.title}</a>
+            </li>
+          ))
+        ) : (
+          <p>Aucune actualité passée.</p>
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default Actu;
