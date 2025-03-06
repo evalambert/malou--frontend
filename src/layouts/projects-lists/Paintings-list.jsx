cf5e7641947bb330cbbd7d50b56a635c4afde7cb
Alice Cadillon <acadillon@gmail.com>
2025-03-06 11:40:10 +0100

    responsive of paintings margin ok

--- src/layouts/projects-lists/Paintings-list.jsx
+++ src/layouts/projects-lists/Paintings-list.jsx
@@ -10,7 +10,10 @@ const PaintingsList = ({ dataPaintings, isOnPaintingPage, targetHref }) => {
             let previousLiWidth = 0;
 
             liTitle.forEach(li => {
-                const viewportWidth = window.innerWidth;
+
+                const viewportWidth = window.matchMedia('(min-width: 48rem)').matches 
+                    ? window.innerWidth - 100 // 100px = md:left-[100px] on item bellow
+                    : window.innerWidth - 30; // --spacing-main-x-mobile (x2)
                 if (previousLiWidth + li.offsetWidth > viewportWidth) {
                     let maxMarginLeft = viewportWidth - li.offsetWidth;
                     li.style.marginLeft = `${maxMarginLeft}px`;
@@ -40,19 +43,19 @@ const PaintingsList = ({ dataPaintings, isOnPaintingPage, targetHref }) => {
     // Render
     return (
         <>
-            
+            {/* ! md:left-[100px] modify, change value const viewportWidth above */} 
             <div 
-                className={`fixed left-[100px] bottom-0 ${!isOnPaintingPage ? 'cursor-pointer' : ''}`} 
+                className={`fixed md:left-[100px] bottom-0 border border-amber-400 ${!isOnPaintingPage ? 'cursor-pointer' : ''}`} 
                 onClick={!isOnPaintingPage ? () => navigate(targetHref, { history: 'push' }) : undefined}
             >
-                <div className={`${!isOnPaintingPage ? 'pointer-events-none' : ''}`}>
-                <PaintingsListCompact dataPaintings={dataPaintings} />
-                    <div className={`max-h-0 overflow-hidden transition-all duration-300 ease-in-out delay-[0.3s] ${
+                <div className={`border border-violet-400  ${!isOnPaintingPage ? 'pointer-events-none' : ''}`}>
+                    <PaintingsListCompact dataPaintings={dataPaintings} />
+                    <div className={`border border-green-400 max-h-0 overflow-hidden transition-all duration-500 ease-in-out delay-[0.2s] ${
                         isOnPaintingPage ? 'max-h-[100vh]' : 'max-h-0'
                     }`}>
                         <ul>
                             {dataPaintings.slice(4).map((painting) => (
-                                <li className="painting-title w-fit" key={painting.id}>
+                                <li className="border border-pink-400 painting-title w-fit block" key={painting.id}>
                                     <a href={`/painting/${painting.slug}`} className="block whitespace-nowrap">{painting.title}</a>
                                 </li>
                             ))}
