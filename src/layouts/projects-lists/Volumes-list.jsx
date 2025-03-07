2868455dde6c6092b8159ff935ceab7813639460
Alice Cadillon <acadillon@gmail.com>
2025-03-07 12:06:15 +0100

    list noue ok

--- src/layouts/projects-lists/Volumes-list.jsx
+++ src/layouts/projects-lists/Volumes-list.jsx
@@ -10,7 +10,7 @@ const VolumesList = ({ dataVolumes, isOnVolumePage, targetHref, lang }) => {
     return (
         <>
             <div
-                className={`border border-amber-400 ${!isOnVolumePage ? "cursor-pointer" : ""
+                className={`pt-list-p-top  ${!isOnVolumePage ? "cursor-pointer" : ""
                     }`}
                 onClick={
                     !isOnVolumePage
@@ -19,18 +19,18 @@ const VolumesList = ({ dataVolumes, isOnVolumePage, targetHref, lang }) => {
                 }
             >
                 <div
-                    className={`border border-violet-400  ${!isOnVolumePage ? "pointer-events-none" : ""
+                    className={`${!isOnVolumePage ? "pointer-events-none" : ""
                         }`}
                 >
                     <div
-                        className={`border border-green-400 max-h-0 overflow-hidden transition-all duration-500 ease-in-out delay-[0.2s] ${isOnVolumePage ? "max-h-[100vh]" : "max-h-0"
+                        className={`max-h-0 overflow-hidden transition-all duration-500 ease-in-out delay-[0.2s] ${isOnVolumePage ? "max-h-[100vh]" : "max-h-0"
                             }`}
                     >
                         {/* Liste Hidden */}
-                        <ul>
+                        <ul className="volume-list-compact flex flex-wrap gap-y-[25px] pb-[25px]">
                             {dataVolumes.slice(3).map((volume) => (
                                 <li
-                                    className="border border-pink-400 volume-title w-fit block"
+                                    className="volume-title w-fit block"
                                     key={volume.id}
                                 >
                                     <a
@@ -45,7 +45,7 @@ const VolumesList = ({ dataVolumes, isOnVolumePage, targetHref, lang }) => {
                         {/* (END) Liste Hidden */}
                     </div>
                     {/* Liste Homepage */}
-                    <ul className="volume-list-compact flex">
+                    <ul className="volume-list-compact flex flex-wrap gap-y-[25px]">
                         {dataVolumes.slice(0, 3).map((volume) => (
                             <li className="volume-title w-fit" key={volume.id}>
                                 <a href={`${lang}/volume/${volume.slug}`} className="block whitespace-nowrap">{volume.title}</a>
