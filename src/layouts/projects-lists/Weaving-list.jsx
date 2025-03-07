94385a24463298149eb8c214bceb96689f0cff9b
Alice Cadillon <acadillon@gmail.com>
2025-03-07 18:00:42 +0100

    anim weaving OK (need fine tunning)

--- src/layouts/projects-lists/Weaving-list.jsx
+++ src/layouts/projects-lists/Weaving-list.jsx
@@ -1,32 +1,64 @@
 import { useEffect } from "react";
+import { navigate } from "astro:transitions/client";
 
-const WeavingList = ({ dataWeaving, lang }) => {
+const WeavingList = ({ dataWeaving, isOnWeavingPage, targetHref, lang }) => {
   useEffect(() => {
     const liTitle = document.querySelectorAll("li.weaving-title");
-    let previousLiWidth = 0;
+    let nextLiWidth = 0;
 
     liTitle.forEach((li) => {
-      li.style.marginLeft = `${previousLiWidth}px`;
-      previousLiWidth = li.offsetWidth;
-      return previousLiWidth;
+      nextLiWidth = li.nextElementSibling ? li.nextElementSibling.offsetWidth : 0;
+      li.style.marginRight = `${nextLiWidth}px`;
+      return nextLiWidth;
     });
 
-    console.log(`Largeur maximale: ${previousLiWidth}px`);
   }, [dataWeaving]);
 
   console.log(dataWeaving);
 
   // Render
   return (
-    <ul className="w-fit">
-      {dataWeaving.map((weaving) => (
-        <li className="weaving-title w-fit" key={weaving.id}>
-          <a href={`/${lang}/weaving/${weaving.slug}`} className="pr-1">
-            {weaving.title}
-          </a>
-        </li>
-      ))}
-    </ul>
+    <>
+      <div className={`fixed right-0 bottom-0 ${!isOnWeavingPage ? "cursor-pointer" : ""
+        }`}
+        onClick={
+          !isOnWeavingPage
+            ? () => navigate(`/${lang}${targetHref}`, { history: "push" })
+            : undefined
+        }>
+        <div
+          className={`grid auto-cols-auto auto-rows-min ${!isOnWeavingPage ? "pointer-events-none" : ""
+            }`}
+        >
+          {/* Liste Homepage */}
+          <ul className="w-fit flex flex-col items-end">
+            {dataWeaving.slice(0, 5).map((weaving) => (
+              <li className="weaving-title w-fit" key={weaving.id}>
+                <a href={`/${lang}/weaving/${weaving.slug}`} className="pr-1">
+                  {weaving.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+          {/* (END) Liste Homepage */}
+          <div className={`max-w-0 max-h-0 overflow-hidden col-start-2 row-start-2 transition-all duration-1000 ease-in-out delay-[0.2s] ${isOnWeavingPage ? "max-w-[100vw] max-h-[100vh]" : ""}`}>
+            {/* Liste Hidden */}
+            {isOnWeavingPage && (
+              <ul className="w-fit flex flex-col items-end">
+                {dataWeaving.slice(5).map((weaving) => (
+                  <li className="weaving-title w-fit" key={weaving.id}>
+                    <a href={`/${lang}/weaving/${weaving.slug}`} className="pr-1">
+                      {weaving.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            )}
+            {/* (END) Liste Hidden */}
+          </div>
+        </div>
+      </div >
+    </>
   );
 };
 
