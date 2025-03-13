1ac48da3243f98eee01eced045ed9050c34c54b4
evalambert <145194730+evalambert@users.noreply.github.com>
2025-03-13 11:11:46 +0100

    fix redirection nav in about page + fix z-index button and nav

--- src/layouts/AboutComponent.jsx
+++ src/layouts/AboutComponent.jsx
@@ -2,28 +2,43 @@
 import { useStore } from "@nanostores/react";
 import { activeComponent, toggleComponent } from "../lib/store.js";
 
-export default function About({ about, lang }) {
+export default function AboutComponent({ about, lang }) {
   const active = useStore(activeComponent);
 
   return (
     <section
-      className={`wrapper-about md:fixed right-0 flex flex-col gap-3 w-full transition-all duration-500 ease-in-out overflow-hidden 
-        ${active === "actu" ? "h-[45px] top-[calc(-100vh)]" : "h-full top-0"}`}
+      className={`wrapper-about md:fixed right-0 flex flex-col gap-3 w-full 
+        ${
+        active === "actu"
+        ? "h-0 top-[calc(-100vh)]"
+        : "h-full top-0"
+        } transition-all duration-500 ease-in-out overflow-hidden `}
     >
       <div className="wrapper--bio md:order-2 flex">
-        <div className="box-fake-nav hidden md:block flex-none w-[170px]"></div>
-        <p className="box-bio">{about.bio}</p>
+        <div className="fake-nav hidden md:block flex-none w-[170px]"></div>
+        <p className="bio">{about.bio}</p>
       </div>
       <div className="wrapper--email md:order-1 flex">
-        <div className="box--fake-nav hidden md:block flex-none w-[270px] h-[131px] "></div>
-        <div className="box--email flex flex-1 justify-center items-center">
+        <div className="fake-nav hidden md:block flex-none w-[270px] h-[131px] "></div>
+        <button
+          className={`fixed top-0 left-[270px] p-6 rotate-[27.28deg] transition-opacity duration-300 
+          ${
+            active === "about"
+              ? "opacity-0 pointer-events-none"
+              : "opacity-100 pointer-events-auto"
+          }`}
+          onClick={toggleComponent}
+        >
+          {lang === "fr" ? "à propos" : "about"}
+        </button>
+        <div className="email flex flex-1 justify-center items-center">
           <p className="h-fit rotate-[-16deg] md:rotate-[10.87deg] py-7 px-4 md:px-0">
             {about.email}
           </p>
         </div>
       </div>
       <div className="wrappper--phone flex md:order-3 justify-end md:justify-start ">
-        {/* <p>{about.studio}, {about.city}</p> */}
+        {/*  <p>{about.studio}, {about.city}</p> */}
         <p className="pt-[31px] ps-[47px] rotate-[15.72deg] md:rotate-45">
           {about.phone}
         </p>
@@ -48,17 +63,6 @@ export default function About({ about, lang }) {
           </p>
         </div>
       </div>
-      <button
-        className={`fixed top-2 right-1/3 transition-opacity duration-300 
-          ${
-            active === "about"
-              ? "opacity-0 pointer-events-none"
-              : "opacity-100 pointer-events-auto"
-          }`}
-        onClick={toggleComponent}
-      >
-        {lang === "fr" ? "à propos" : "about"}
-      </button>
     </section>
   );
 }
