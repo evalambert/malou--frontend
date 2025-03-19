3fdeb719189efa5cc08fd58f11806e27d7bc468c
evalambert <145194730+evalambert@users.noreply.github.com>
2025-03-19 16:32:49 +0100

    wip accodion read project

--- src/components/AccordionReadProject.jsx
+++ src/components/AccordionReadProject.jsx
@@ -1,3 +1,5 @@
+// src/components/AccordionReadProject.jsx
+
 import { useState } from 'react';
 import '../assets/styles/triangleShapeText.css';
 
@@ -12,31 +14,35 @@ export default function AccordionReadProject({
 }) {
     const [isOpen, setIsOpen] = useState(false);
     return (
-        <div className='wrapper-accordion-read-project w-full fixed z-[999] bottom-0 left-0 right-0 flex flex-col justify-center items-center bg-black border-2 border-red-500'>
-            <button
-                onClick={() => setIsOpen(!isOpen)}
-                className='w-full p-4 text-white border-2 border-blue-600'
-            >
-                {lang === 'fr' ? 'Lire' : 'Read'}
-            </button>
-
             <div
-                className={`bloc-description w-full transition-[max-height] duration-500 ease-liear overflow-hidden text-center text-white 
-                    ${isOpen ? 'max-h-[calc(100vh)]' : 'max-h-0'}`}
+                className={`wrapper-accordion-read-project w-full absolute z-[999] left-0 bg-black border-2 border-white 
+                    ${
+                        isOpen
+                            ? 'h-full top-[calc(50%)]'
+                            : 'h-[61px] top-[calc(100%-61px)] overflow-hidden'
+                    } transition-all duration-500 ease-in-out`}
             >
-                <div className='triangle-right'></div>
-                <div className='triangle-left'></div>
-                <div className='description-project'>
-                    <p>{description}</p>
-                    <p>
-                        {technique}, {materials}
-                    </p>
-                    <p>
-                        {width} x {height} cm
-                    </p>
-                    <p>{year}</p>
+                <button
+                    onClick={() => setIsOpen(!isOpen)}
+                    className='w-full p-4 text-white border-2 border-red-500'
+                >
+                    {lang === 'fr' ? 'Lire' : 'Read'}
+                </button>
+
+                <div className='bloc-description text-center text-white border-2 border-green-500'>
+                    {/* <div className='triangle-right'></div>
+                <div className='triangle-left'></div> */}
+                    <div className='description-project'>
+                        <p>{description}</p>
+                        <p>
+                            {technique}, {materials}
+                        </p>
+                        <p>
+                            {width} x {height} cm
+                        </p>
+                        <p>{year}</p>
+                    </div>
                 </div>
             </div>
-        </div>
     );
 }
