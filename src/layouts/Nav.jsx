688e2bd5b84cc9ab811a83f8ea582bd39e81ff09
evalambert <145194730+evalambert@users.noreply.github.com>
2025-03-21 18:25:41 +0100

    fix navigation malou raulin nav link

--- src/layouts/Nav.jsx
+++ src/layouts/Nav.jsx
@@ -1,67 +1,68 @@
-import { useStore } from "@nanostores/react";
-import { textWhite } from "../lib/store.js";
+import { useStore } from '@nanostores/react';
+import { textWhite } from '../lib/store.js';
 
 const Nav = ({ lang, currentPath, className }) => {
-  const isTextWhite = useStore(textWhite);
+    const isTextWhite = useStore(textWhite);
 
-  // Assurez-vous que la valeur par défaut est définie de manière cohérente
+    const isHomePage =
+        currentPath === `/${lang}` || currentPath === `/${lang}/`;
 
-  const isHome =
-    currentPath === `/${lang}` ||
-    currentPath === `/${lang}/` ||
-    currentPath === `/${lang}/about/`;
+    const isAboutPage = currentPath === `/${lang}/about/`;
 
-  const getLinkClass = (path) => {
-    if (isHome) return "nav-li nav-home";
-    return `nav-li ${currentPath.startsWith(path) ? "nav-on" : "nav-off"}`;
-  };
+    const getLinkClass = (path) => {
+        if (isHomePage) return 'nav-li nav-home';
+        return `nav-li ${currentPath.startsWith(path) ? 'nav-on' : 'nav-off'}`;
+    };
 
+    // Redirection link for "malou raulin"
+    let destination = `/${lang}/`;
+    if (isHomePage) {
+        destination = `/${lang}/about/`;
+    } else if (isAboutPage) {
+        destination = `/${lang}/`;
+    }
 
-
-
-  const isAboutPage = currentPath === `/${lang}/about/`;
-
-  // Render
-  return (
-    <>
-      <div className={`nav-wrapper ${className} flex gap-[10px] pt-[6px] ${isTextWhite ? 'text-white' : 'text-black'}`}>
-
-        <a href={isAboutPage ? `/${lang}/` : `/${lang}/about/`}>
-          malou raulin
-        </a>
-
-        <nav>
-          <ul className="flex flex-col">
-            <li className={getLinkClass(`/${lang}/weaving/`)}>
-              <a href={`/${lang}/weaving/`}>
-                {lang === "fr" ? "tisse," : "weaving,"}
-              </a>
-            </li>
-            <li className={getLinkClass(`/${lang}/volume/`)}>
-              <a href={`/${lang}/volume/`}>
-                {lang === "fr" ? "noue," : "volume,"}
-              </a>
-            </li>
-            <li className={getLinkClass(`/${lang}/painting/`)}>
-              <a href={`/${lang}/painting/`}>
-                {lang === "fr" ? "peint," : "paint,"}
-              </a>
-            </li>
-            <li className={getLinkClass(`/${lang}/poetry/`)}>
-              <a href={`/${lang}/poetry/`}>
-                {lang === "fr" ? "écrit," : "write,"}
-              </a>
-            </li>
-            <li className={getLinkClass(`/${lang}/vitrail/`)}>
-              <a href={`/${lang}/vitrail/`}>
-                {lang === "fr" ? "cisèle," : "vitrail,"}
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </>
-  );
+    // Render
+    return (
+        <>
+            <div
+                className={`nav-wrapper ${className} flex gap-[10px] pt-[6px] ${
+                    isTextWhite ? 'text-white' : 'text-black'
+                }`}
+            >
+                <a href={destination}>malou raulin</a>
+                <nav>
+                    <ul className='flex flex-col'>
+                        <li className={getLinkClass(`/${lang}/weaving/`)}>
+                            <a href={`/${lang}/weaving/`}>
+                                {lang === 'fr' ? 'tisse,' : 'weaving,'}
+                            </a>
+                        </li>
+                        <li className={getLinkClass(`/${lang}/volume/`)}>
+                            <a href={`/${lang}/volume/`}>
+                                {lang === 'fr' ? 'noue,' : 'volume,'}
+                            </a>
+                        </li>
+                        <li className={getLinkClass(`/${lang}/painting/`)}>
+                            <a href={`/${lang}/painting/`}>
+                                {lang === 'fr' ? 'peint,' : 'paint,'}
+                            </a>
+                        </li>
+                        <li className={getLinkClass(`/${lang}/poetry/`)}>
+                            <a href={`/${lang}/poetry/`}>
+                                {lang === 'fr' ? 'écrit,' : 'write,'}
+                            </a>
+                        </li>
+                        <li className={getLinkClass(`/${lang}/vitrail/`)}>
+                            <a href={`/${lang}/vitrail/`}>
+                                {lang === 'fr' ? 'cisèle,' : 'vitrail,'}
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </>
+    );
 };
 
 export default Nav;
