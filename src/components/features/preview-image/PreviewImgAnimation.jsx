8b577eec1c1a76d60e295d954f34a11a8c1832fc
Alice Cadillon <acadillon@gmail.com>
2025-03-27 09:07:07 +0100

    retouche preview img

--- src/components/features/preview-image/PreviewImgAnimation.jsx
+++ src/components/features/preview-image/PreviewImgAnimation.jsx
@@ -3,18 +3,18 @@ import { useState, useEffect } from 'react';
 const PreviewImgAnimation = ({ medias = [] }) => {
 
     const [showPreview, setShowPreview] = useState(false);
-    const [hidePreview, setHidePreview] = useState(false);
+    // const [hidePreview, setHidePreview] = useState(false);
 
     useEffect(() => {
         setShowPreview(true);
     }, []);
-    useEffect(() => {
-        const timeout = setTimeout(() => {
-            setHidePreview(true);
-        }, 1200); // Délai de 0,5s avant d'afficher Swiper
+    // useEffect(() => {
+    //     const timeout = setTimeout(() => {
+    //         setHidePreview(true);
+    //     }, 1010); // Délai de 0,5s avant d'afficher Swiper
 
-        return () => clearTimeout(timeout);
-    }, []);
+    //     return () => clearTimeout(timeout);
+    // }, []);
 
     // Render
     return (
@@ -26,16 +26,16 @@ const PreviewImgAnimation = ({ medias = [] }) => {
                         width: 350px;
                         height: 300px;
                     }
-                    100% {
+                    100% {    
                         width: 100vw;
                         height: 100vh;
                     }
                 }
                 `}
             </style>
-            
-            <div className={`fixed top-0 left-0 w-screen h-screen flex items-center justify-center ${hidePreview ? 'hidden' : ''}`}>
-                <div className={` opacity-100 transition-all duration-300 w-[350px] h-[300px]`} style={{ animation: showPreview ? 'preview-img-animation 1s ease' : 'none' }}>
+
+            <div className={`fixed top-0 left-0 w-screen h-screen flex items-center justify-center !z-0 `}>
+                <div className={` opacity-100 transition-all duration-350 w-[350px] h-[300px] border-2 border-violet-500`} style={{ animation: showPreview ? 'preview-img-animation 1s ease' : 'none' }}>
                     <img key={medias[0]?.url} src={medias[0]?.url} alt={medias[0]?.title} class="w-full h-full object-cover" />
                 </div>
             </div>
