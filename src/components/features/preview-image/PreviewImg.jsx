7f9b0e8007e4afae0a1917a0daf85ff4d0a393a3
Alice Cadillon <acadillon@gmail.com>
2025-03-27 10:57:51 +0100

    entering animation OK on vitrail :-)

--- src/components/features/preview-image/PreviewImg.jsx
+++ src/components/features/preview-image/PreviewImg.jsx
@@ -1,11 +1,32 @@
 
 const PreviewImg = () => {
 
-
     // Render
     return (
         <>
-            <img className="w-full h-full object-cover dynamic-image" src="/" alt="preview image"></img>
+            <style>
+                {`
+                @keyframes preview-img-animation {
+                    0% {
+                        width: 350px;
+                        height: 300px;
+                    }
+                    100% {    
+                        width: 100vw;
+                        height: 100vh;
+                    }
+                }
+                body.on-slug-page .preview-image--wrapper {
+                    animation: preview-img-animation 1s ease forwards;
+                }
+            `}
+            </style>
+            <div className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center !z-0">
+                <div className="preview-image--wrapper opacity-0 transition-all duration-350 w-[350px] h-[300px]">
+
+                    <img className="preview-img w-full h-full object-cover dynamic-image " src="/" alt="preview image" data-persist></img>
+                </div>
+            </div>
         </>
     );
 };
---
+++ src/utils/preview-img.js
@@ -0,0 +1,23 @@
+export const handleMouseEnter = (imageUrl) => {    
+    const imageElement = document.querySelector('.dynamic-image');
+    const wrapperElement = document.querySelector('.preview-image--wrapper');
+    if (!document.body.classList.contains('on-slug-page')) {
+        if (imageElement) {
+            imageElement.src = imageUrl;
+            imageElement.dataset.lastImage = imageUrl;
+        }
+        if (wrapperElement) {
+            wrapperElement.style.opacity = '1';
+        }
+    }
+};
+
+export const handleMouseLeave = () => {
+    const wrapperElement = document.querySelector('.preview-image--wrapper');
+    if (!document.body.classList.contains('on-slug-page')) {
+        if (wrapperElement && !wrapperElement.classList.contains('preview-image--wrapper-visible')) {
+            wrapperElement.style.opacity = '0';
+        }
+    }
+};
+
