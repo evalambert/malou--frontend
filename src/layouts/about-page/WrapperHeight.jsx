4760300b5256eff8b3d126bdb4d8b020861d9176
evalambert <145194730+evalambert@users.noreply.github.com>
2025-04-09 18:10:57 +0200

    fixed consol.log error on aboutPage

--- src/layouts/about-page/WrapperHeight.jsx
+++ src/layouts/about-page/WrapperHeight.jsx
@@ -1,4 +1,4 @@
-// src/components/WrapperHeight.jsx
+/* // src/components/WrapperHeight.jsx
 import { useStore } from '@nanostores/react';
 import {
     activeComponent,
@@ -12,6 +12,43 @@ export default function WrapperHeight() {
     const actuHeight = useStore(heightActu);
 
     const computedHeight = active === 'about' ? aboutHeight : actuHeight;
+
+    return (
+        <style>
+            {`
+        @media (max-width: 768px) {
+          .wrapper-about-actus {
+            height: ${computedHeight}px;
+            transition: all 0.5s ease-in-out;
+          }
+        }
+      `}
+        </style>
+    );
+}
+ */
+
+import { useStore } from '@nanostores/react';
+import { useEffect, useState } from 'react';
+import {
+    activeComponent,
+    heightAbout,
+    heightActu,
+} from '../../assets/scripts/lib/store.js';
+
+export default function WrapperHeight() {
+    const [isClient, setIsClient] = useState(false);
+    const active = useStore(activeComponent);
+    const aboutHeight = useStore(heightAbout);
+    const actuHeight = useStore(heightActu);
+
+    const computedHeight = active === 'about' ? aboutHeight : actuHeight;
+
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
+    if (!isClient) return null;
 
     return (
         <style>
