3796aadfddf1cd5bdd545521d64560c5f1a934dc
evalambert <145194730+evalambert@users.noreply.github.com>
2025-04-09 18:12:46 +0200

    refix

--- src/layouts/about-page/WrapperHeight.jsx
+++ src/layouts/about-page/WrapperHeight.jsx
@@ -1,33 +1,4 @@
-/* // src/components/WrapperHeight.jsx
-import { useStore } from '@nanostores/react';
-import {
-    activeComponent,
-    heightAbout,
-    heightActu,
-} from '../../assets/scripts/lib/store.js';
-
-export default function WrapperHeight() {
-    const active = useStore(activeComponent);
-    const aboutHeight = useStore(heightAbout);
-    const actuHeight = useStore(heightActu);
-
-    const computedHeight = active === 'about' ? aboutHeight : actuHeight;
-
-    return (
-        <style>
-            {`
-        @media (max-width: 768px) {
-          .wrapper-about-actus {
-            height: ${computedHeight}px;
-            transition: all 0.5s ease-in-out;
-          }
-        }
-      `}
-        </style>
-    );
-}
- */
-
+// src/components/WrapperHeight.jsx
 import { useStore } from '@nanostores/react';
 import { useEffect, useState } from 'react';
 import {
