19d22619b07498e56cba93bb4c900e9360b58671
evalambert <145194730+evalambert@users.noreply.github.com>
2025-04-09 13:52:38 +0200

    Réinitialisation de activeComponent à 'about' à chaque fois que AboutPage est montée

--- src/assets/scripts/lib/store.js
+++ src/assets/scripts/lib/store.js
@@ -1,22 +1,16 @@
 // src/lib/store.js
 import { atom } from 'nanostores';
 
-// ——————————————————————————————————————————————————————————————————
-// Variables
-// ——————————————————————————————————————————————————————————————————
-
 // État actif ("about" ou "actu")
 export const activeComponent = atom('about');
+
 
 // Hauteurs des sections
 export const heightAbout = atom(0);
 export const heightActu = atom(0);
 
-// ——————————————————————————————————————————————————————————————————
-// Functions
-// ——————————————————————————————————————————————————————————————————
-
-// Actu/About basculer entre les deux états
+// Fonctions pour basculer entre les sections
 export const toggleComponent = () => {
     activeComponent.set(activeComponent.get() === 'about' ? 'actu' : 'about');
 };
+
