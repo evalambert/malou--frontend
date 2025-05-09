491a7f046705d38bdfd3bdda651a15e00a060129
Alice Cadillon <acadillon@gmail.com>
2025-05-09 15:36:26 +0200

    masqué hidden list weaving sur la home ok

--- src/layouts/projects-lists/WeavingList.jsx
+++ src/layouts/projects-lists/WeavingList.jsx
@@ -330,7 +330,7 @@ const WeavingList = ({
                     {/* (END) Liste Homepage */}
 
                     <div
-                        className={`hidden-list-weaving w-[100%] overflow-hidden transition-all delay-[0.2s] duration-1000 ease-in-out md:w-fit`}
+                        className={`hidden-list-weaving w-[100%] overflow-hidden transition-all delay-[0.2s] duration-1000 ease-in-out md:w-fit ${!isOnWeavingPage ? 'opacity-0' : 'opacity-100'}`}
                     >
                         {/* Liste Hidden */}
                         {/* {isOnWeavingPage && ( */}
